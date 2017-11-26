512e48d9884b812d6aa40587f84ad9fc40dd4845
nosrac77 <carson.newton@outlook.com>
2017-11-25 16:18:53 -0800

    added and refactored tests, added to models.py to include choice fields.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -5,22 +5,54 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-"""Sample models for a camera."""
-CAMERA_MODELS = [('NikonD3300', 1), ('CanonT6i', 2), ('Canon5dMarkIII', 3)]
-
-
 class ImagerProfile(models.Model):
     """Profile template for a created image."""
+
+    NIKOND3300 = 'NKD3300'
+    CANONT6I = 'CNT6I'
+    CANON5DMARKIII = 'CNMKD5III'
+    SONYALPHAA99II = 'SNYA99II'
+
+    CAMERA_MODELS = (
+                    (NIKOND3300, 'NikonD3300'),
+                    (CANONT6I, 'CanonT6i'),
+                    (CANON5DMARKIII, 'Canon5dMarkIII'),
+                    (SONYALPHAA99II, 'SonyAlphaA99II')
+                    )
+
+    ULTIMATE = 'ULT'
+    MEGA = 'MEGA'
+    BASIC = 'BSC'
+
+    SERVICES = (
+               (ULTIMATE, '20 photos, provided lighting equipment, additional photo editing post-production.'),
+               (MEGA, '15 photos, provided lighting equipment, 5 free prints of your choice.'),
+               (BASIC, '10 photos, 3 free prints of your choice.')
+               )
+
+    SEVENTIES = '70s'
+    NOIR = 'NOIR'
+    BOKEH = 'BOKEH'
+    STUDIO = 'STUDIO'
+    STANDARD = 'STD'
+
+    STYLES = (
+             (SEVENTIES, 'Classic retro style with filters to match.'),
+             (NOIR, 'Bold black and white photos.'),
+             (BOKEH, 'Blurry background with subject in focus.'),
+             (STUDIO, 'Profile shots with bright lighting and white backdrop.'),
+             (STANDARD, 'Regular shots with no filters.')
+             )
 
     user = models.OneToOneField(User)
     website = models.CharField(max_length=180)
     location = models.CharField(max_length=50)
     commission = models.FloatField(max_length=20)
-    camera = models.CharField(max_length=20, choices=CAMERA_MODELS, default='1')
-    services = models.TextField(max_length=2000)
+    camera = models.CharField(max_length=20, choices=CAMERA_MODELS, default=CANONT6I)
+    services = models.TextField(max_length=2000, choices=SERVICES, default=MEGA)
     bio = models.TextField(max_length=2000)
     phone = models.CharField(max_length=14)
-    photo_styles = models.TextField(max_length=400)
+    photo_styles = models.TextField(max_length=400, choices=STYLES, default=STANDARD)
 
     @property
     def active(self):
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -2,27 +2,49 @@
 
 from django.test import TestCase
 from imager_profile.models import ImagerProfile
-import factory
 from django.contrib.auth.models import User
+import factory
 
 
-class Userfactory(factory.django.DjangoModelFactory):
-    """Create classes for testing user."""
+class UserFactory(factory.django.DjangoModelFactory):
+    """Create classes for testing User."""
 
     class Meta:
-        """Create a class for testing user."""
+        """Create instance of User object."""
 
         model = User
+
+    username = 'BobbyG'
+    password = 'Bobbybethebest'
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    """Create classes for testing ImagerProfile."""
+
+    class Meta:
+        """Create instance of ImagerProfile."""
+
+        model = ImagerProfile
+
+    user = factory.SubFactory(UserFactory)
+    website = 'google.com'
+    location = 'seattle'
+    commission = 50.00
+    camera = 'Nikon'
+    services = 'I do stuff'
+    bio = 'I am a person'
+    phone = '2062427983'
+    photo_styles = 'all the styles'
 
 
 class ImagersiteTests(TestCase):
     """Test suite for testing profiles."""
 
-    def test_that_imager_profile_creates_profile(self):
-        """Test create new user."""
-        bob = User(username='fun', password='stuff')
-        bob.save()
-        profile = ImagerProfile(user=bob, website='google.com', location='seattle', commission=50.00, camera='Nikon', services='I do stuff', bio='I am a person', phone='2062427983', photo_styles='all the styles')
+    def test_that_imager_profile_creates_profile_with_given_user(self):
+        """Test creation of ImagerProfile creates a profile with correct
+        given values, including access to User username and password."""
+
+        profile = ProfileFactory()
         self.assertEquals(profile.location, 'seattle')
         self.assertEquals(profile.commission, 50.00)
         self.assertEquals(profile.camera, 'Nikon')
@@ -30,10 +52,23 @@ class ImagersiteTests(TestCase):
         self.assertEquals(profile.bio, 'I am a person')
         self.assertEquals(profile.phone, '2062427983')
         self.assertEquals(profile.photo_styles, 'all the styles')
-        self.assertEquals(profile.is_active, True)
-        print(type(bob))
-        print('--------')
-        print(type(profile.active))
-        self.assertEqual(profile.active.values()[0]['username'], 'fun')
+        self.assertEquals(profile.user.username, 'BobbyG')
+        self.assertEquals(profile.user.password, 'Bobbybethebest')
 
-# '<QuerySet [<User: fun>]>'
+    def test_that_imager_profile_methods_return_correct_values(self):
+        """Test ImagerProfile methods return correct values."""
+
+        profile = ProfileFactory()
+        self.assertEquals(profile.is_active, True)
+        self.assertEqual(profile.active.values()[0]['username'], 'BobbyG')
+        self.assertEqual(profile.active.values()[0]['password'], 'Bobbybethebest')
+
+    def test_imager_profile_camera_styles_services_default_values(self):
+        """Test default values for camera, photo_styles, and services are
+        given to new ImagerProfile if no values entered."""
+
+        bob = UserFactory()
+        profile = ImagerProfile(user=bob, website='google.com', location='seattle', commission=100.00, bio='I am a person', phone='2062427983')
+        self.assertEquals(profile.camera, 'CNT6I')
+        self.assertEquals(profile.services, 'MEGA')
+        self.assertEquals(profile.photo_styles, 'STD')
